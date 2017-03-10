5826ad117c4706f43084fa85efc5a62fd28b0246
Todd Wolfson <todd@twolfson.com>
2017-03-09 23:49:26 -0800

    Got working fix

--- spec/api-remote-spec.js
+++ spec/api-remote-spec.js
@@ -5,6 +5,28 @@ describe('remote module', function () {
   describe('remoteMemberFunction.toString()', function () {
     it.only('returns string representation of original function', function () {
       var write = electron.clipboard.write
+      var write = new Proxy(function x () {}, {
+        set: (target, property, value, receiver) => {
+          if (property !== 'ref') loadRemoteProperties()
+          target[property] = value
+          return true
+        },
+        get: (target, property, receiver) => {
+          if (property === 'toString') { return target.toString.bind(target); }
+          if (!target.hasOwnProperty(property)) loadRemoteProperties()
+          return target[property]
+        },
+        ownKeys: (target) => {
+          loadRemoteProperties()
+          return Object.getOwnPropertyNames(target)
+        },
+        getOwnPropertyDescriptor: (target, property) => {
+          let descriptor = Object.getOwnPropertyDescriptor(target, property)
+          if (descriptor != null) return descriptor
+          loadRemoteProperties()
+          return Object.getOwnPropertyDescriptor(target, property)
+        }
+      });
       assert(write.toString().startsWith('function'))
     })
   })
